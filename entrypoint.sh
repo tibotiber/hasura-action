6d6fcbf69731ccb3cd30d9d15cd8e596c2cfb52e
Thibaut Tiberghien <thibaut@smplrspace.com>
2019-11-15 14:56:41 +0800

    add print for debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,13 +8,17 @@ if [ -z "$HASURA_ENDPOINT" ]; then
 fi
 
 command="hasura $* --endpoint $HASURA_ENDPOINT"
+command_to_print="hasura $* --endpoint $HASURA_ENDPOINT"
 
 if [ -n "$HASURA_ADMIN_SECRET" ]; then
     command="$command --admin-secret $HASURA_ADMIN_SECRET"
+    command_to_print="$command --admin-secret ***"
 fi
 
 if [ -n "$HASURA_WORKDIR" ]; then
     cd $HASURA_WORKDIR
 fi
+
+echo "Executing '$command_to_print' from ${HASURA_WORKDIR:-./}"
 
 sh -c $command
