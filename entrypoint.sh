f65c79c9bef49254a95668ea849eb69d7034f1f2
David Bellotti <dbellotti@gmail.com>
2021-08-06 14:22:48 -0700

    Syncs the cli with target hasura engine version
    - uses HASURA_ENGINE_VERSION then DETECTED_HASURA_ENGINE_VERSION if
      different from CURRENT_HASURA_CLI_VERSION

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,12 @@ if [ -z "$HASURA_ENDPOINT" ]; then
     exit 126
 fi
 
+CURRENT_HASURA_CLI_VERSION=$(hasura version --skip-update-check | jq -r .version)
+DETECTED_HASURA_ENGINE_VERSION=$(curl -s "$HASURA_ENDPOINT"/v1/version \
+  | jq .version \
+  | awk '{split($0,a,"-"); print a[1]}' \
+  | awk '{split($0,a,"\""); print a[2]}')
+
 command="hasura $* --endpoint '$HASURA_ENDPOINT'"
 
 if [ -n "$HASURA_ADMIN_SECRET" ]; then
@@ -24,6 +30,8 @@ fi
 
 if [ -n "$HASURA_ENGINE_VERSION" ]; then
     hasura update-cli --version $HASURA_ENGINE_VERSION
+elif [ "$CURRENT_HASURA_CLI_VERSION" != "$DETECTED_HASURA_ENGINE_VERSION" ]; then
+    hasura update-cli --version $DETECTED_HASURA_ENGINE_VERSION
 fi
 
 
