5f8258d41730789e8739b4d40d86442b37c01559
David Bellotti <dbellotti@gmail.com>
2021-08-11 09:22:13 -0700

    Moves some logic around so it is in one spot
    - also skip extra work if the desired hasura cli version
      is specified

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,12 +7,6 @@ if [ -z "$HASURA_ENDPOINT" ]; then
     exit 126
 fi
 
-CURRENT_HASURA_CLI_VERSION=$(hasura version --skip-update-check | jq -r .version)
-DETECTED_HASURA_ENGINE_VERSION=$(curl -s "$HASURA_ENDPOINT"/v1/version \
-  | jq .version \
-  | awk '{split($0,a,"-"); print a[1]}' \
-  | awk '{split($0,a,"\""); print a[2]}')
-
 command="hasura $* --endpoint '$HASURA_ENDPOINT'"
 
 if [ -n "$HASURA_ADMIN_SECRET" ]; then
@@ -30,8 +24,17 @@ fi
 
 if [ -n "$HASURA_ENGINE_VERSION" ]; then
     hasura update-cli --version $HASURA_ENGINE_VERSION
-elif [ -n "$DETECTED_HASURA_ENGINE_VERSION" ] && [ "$CURRENT_HASURA_CLI_VERSION" != "$DETECTED_HASURA_ENGINE_VERSION" ]; then
-    hasura update-cli --version $DETECTED_HASURA_ENGINE_VERSION
+else
+  DETECTED_HASURA_ENGINE_VERSION=$(curl -s "$HASURA_ENDPOINT"/v1/version \
+    | jq .version \
+    | awk '{split($0,a,"-"); print a[1]}' \
+    | awk '{split($0,a,"\""); print a[2]}')
+
+  CURRENT_HASURA_CLI_VERSION=$(hasura version --skip-update-check | jq -r .version)
+
+  if [ -n "$DETECTED_HASURA_ENGINE_VERSION" ] && [ "$DETECTED_HASURA_ENGINE_VERSION" != "$CURRENT_HASURA_CLI_VERSION" ]; then
+      hasura update-cli --version $DETECTED_HASURA_ENGINE_VERSION
+  fi
 fi
 
 # secrets can be printed, they are protected by Github Actions
